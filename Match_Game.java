767bef4b499251fe6533f1559076cd665860a402
SiennaE <sienna.evangelista@kennedyhs.org>
2021-03-01 23:40:01 -0800

    added stuff

--- Match_Game.java
+++ Match_Game.java
@@ -78,6 +78,42 @@ public class Match_Game
             System.out.println(" ");
         }
     }
+    static void printSymbols(String[][] cardGrid, String[][] gridWhole, String[] horizontalCoords, String[] verticalCoords, String letterGuess1, String letterGuess2, int numberGuess1, int numberGuess2, int letter1, int letter2, String[][] gridSymbols)
+    {
+        for (int j = 1; j < 6; j++)
+        {
+            gridWhole[0][j] = horizontalCoords[j];
+        }
+        
+        for (int s = 1; s < 5; s++)
+        {
+            gridWhole[s][0] = verticalCoords[s];
+        }
+        
+        for (int a = 1; a < 5; a++)
+        {
+            for (int b = 1; b < 6; b++)
+            {
+                gridWhole[a][b] = cardGrid[a-1][b-1];
+            }
+        }
+        
+        gridWhole[letter1][numberGuess1] = gridSymbols[letter1-1][numberGuess1-1];
+        gridWhole[letter2][numberGuess2] = gridSymbols[letter2-1][numberGuess2-1];
+        
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                System.out.print(gridWhole[i][j] + "     ");
+            }
+            System.out.println(" ");
+            System.out.println(" ");
+        }
+        
+        
+    }
+    //change so if grid symbols !" " then repeat the random number generator
     static String[][] randomSpace(String[] symbols, String[] letters, String[][] gridSymbols, String[][] duplicate)
     {
         int counter = 0;
@@ -90,7 +126,7 @@ public class Match_Game
         String[] noNoSpaces = {"0","0","0"};
         int v = 0;
         
-        while (counter < 3)
+        while (counter < 10)
         {
             int x = 0;
             while (x < 2)
@@ -109,22 +145,59 @@ public class Match_Game
                 x++;
             }
                 
-                noNoSpaces[v] = gridSymbols[counter][0];
-                v++;
+            noNoSpaces[v] = gridSymbols[counter][0];
+            v++;
                 
-                counter++;
+            counter++;
         }
         
         return gridSymbols;
     }
     //ask user
-    static void userGuess()
+    static void revealSymbols(String ready, Scanner input, String letterGuess1, String letterGuess2, int numberGuess1, int numberGuess2, int letter1, int letter2, String[][] gridSymbols, String[] verticalCoords, String[] horizontalCoords, String[][] gridWhole, String[][] cardGrid)
     {
+        if (letterGuess1.equals("A"))
+        {
+            letter1 = 1;
+        }
+        else if (letterGuess1.equals("B"))
+        {
+            letter1 = 2;
+        }
+        else if (letterGuess1.equals("C"))
+        {
+            letter1 = 3;
+        }
+        else if (letterGuess1.equals("D"))
+        {
+            letter1 = 4;
+        }
+        if (letterGuess2.equals("A"))
+        {
+            letter2 = 1;
+        }
+        else if (letterGuess2.equals("B"))
+        {
+            letter2 = 2;
+        }
+        else if (letterGuess2.equals("C"))
+        {
+            letter2 = 3;
+        }
+        else if (letterGuess2.equals("D"))
+        {
+            letter2 = 4;
+        }
         
-    }
-    static void revealSymbols()
-    {
+        printSymbols(cardGrid, gridWhole, horizontalCoords, verticalCoords, letterGuess1, letterGuess2, numberGuess1, numberGuess2, letter1, letter2, gridSymbols);
         
+        if (gridSymbols[letter1-1][numberGuess1-1].equals(gridSymbols[letter2][numberGuess2]))
+        {
+            System.out.println("Congrats! You found a pair.");
+        }
+        
+        System.out.println("Type anything when you are ready to continue");
+        ready = input.next();
     }
     static void matched()
     {
@@ -139,12 +212,24 @@ public class Match_Game
             
             String ynAnswer;
             boolean tOrF = false;
+            String letterGuess1;
+            int letter1 = 0;
+            int numberGuess1;
+            String letterGuess2;
+            int letter2 = 0;
+            int numberGuess2;
+            String ready = " ";
+            int countMatch = 0;
             
             String[] horizontalCoords = {" ", "1 ", "2 ", "3 ", "4 ", "5"};
             String[] verticalCoords = {" ", "A", "B", "C", "D"};
+            //hidden board containing the locations of the symbols
             String[][] gridSymbols = {{" "," "," "," "," "},{" "," "," "," "," "},{" "," "," "," "," "},{" "," "," "," "," "},{" "," "," "," "," "}};
+            //just for show
             String[][] cardGrid = {{"☐","☐","☐","☐","☐"},{"☐","☐","☐","☐","☐"},{"☐","☐","☐","☐","☐"},{"☐","☐","☐","☐","☐"},{"☐","☐","☐","☐","☐"}};
+            //print the coordinates and boxes
             String[][] gridWhole = {{" ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " "}};
+            //make sure no repeating spaces (maybe delete later)
             String[][] duplicate = {{" ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " "}, {" ", " ", " ", " ", " ", " "}};
             
             String[] symbols = {"♕", "♫", "★", "☀", "▲", "☾", "♣", "♦", "♥", "♠"};
@@ -175,9 +260,32 @@ public class Match_Game
             print(cardGrid, gridWhole, horizontalCoords, verticalCoords);
             
             //assign symbols to grid spaces
+            //randomSpace(symbols,letters,gridSymbols,duplicate);
             
             //LOOP ask user to pick two grid spaces
             //      reveal what is under the spaces
+            
+            //do {
+                System.out.println("Guess two spaces to compare by inputting a combination of vertical and horizontal coordinates.");
+                System.out.println(" ");
+                System.out.println(" ");
+                System.out.println("Space 1");
+                System.out.println(" ");
+                System.out.print("Vertical Coordinate (capital letter A-D): ");
+                letterGuess1 = input.next();
+                System.out.print("Horizontal Coordinate (number 1-5): ");
+                numberGuess1 = input.nextInt();
+                System.out.println(" ");
+                System.out.println(" ");
+                System.out.println("Space 2");
+                System.out.println(" ");
+                System.out.print("Vertical Coordinate (capital letter A-D): ");
+                letterGuess2 = input.next();
+                System.out.print("Horizontal Coordinate (number 1-5): ");
+                numberGuess2 = input.nextInt();
+                
+                revealSymbols(ready,input,letterGuess1,letterGuess2,numberGuess1,numberGuess2,letter1,letter2,gridSymbols,verticalCoords,horizontalCoords,gridWhole,cardGrid);
+            //} while ();
             
             //user matches all symbols
             
